617f4f2a833c3379f415cd7c9565c37828e2c459
fernando.odilon@aluno.senai.br <maria.j.santos108@aluno.senai.br>
2025-08-08 14:43:11 -0300

    CREATE: Sistema de Listagem de Tarefas

---
+++ funcoes.py
@@ -0,0 +1,86 @@
+
+class Tarefa:
+
+    def __init__(self):
+        self.lista_de_tarefas = []
+
+
+
+    def adicionar_tarefa(self):
+        tarefa = input('Insira sua Tarefa: ').upper()
+        if tarefa.isnumeric():
+            print('\nUma tarefa não pode ser apenas números.')
+        else:
+            for x in self.lista_de_tarefas:
+                if x['nome'] == tarefa:
+                    print('\nEsta tarefa já existe na lista.')
+                    return
+            self.lista_de_tarefas.append({'nome': tarefa, 'status': ' '})
+            print('\nTarefa adicionada com sucesso!')
+
+
+    def remover_tarefas(self):
+        if len(self.lista_de_tarefas) == 0:
+            print('\nSua lista está vazia')
+        else:
+            tarefa = input('\nTarefa a ser removida: ').upper()
+            for x in self.lista_de_tarefas:
+                if x['nome'] == tarefa:
+                    self.lista_de_tarefas.remove(x)
+                    print('\nTarefa removida com sucesso!')
+                
+                else:
+                    print('Esta tarefa não existe')
+            
+
+
+
+    def concluir_tarefa(self):
+        tarefa = input('Insira a tarefa que deseja concluir: ')
+        for x in self.lista_de_tarefas:
+                if x['nome'] == tarefa.upper():
+                    x['status'] = 'x'
+        pass
+    def mostra_tarefas(self):
+        print('\nTAREFAS:\n')
+        for x in self.lista_de_tarefas:
+            print(f'[{x['status']}] {x['nome']}')
+
+    def trasncrever_lista(self):
+        try:
+            with open('tarefas.txt', 'r') as f:
+                for x in f:
+                    x.lstrip()
+                    if x.startswith('x'):
+                        self.lista_de_tarefas.append({'nome':x.strip("x\n "), 'status':'x'})
+                    else:
+                        self.lista_de_tarefas.append({'nome':x.strip("x\n "), 'status':' '})
+
+        except FileNotFoundError:
+            with open('tarefas.txt', 'a') as f:
+                pass
+
+
+    def atualizar_lista(self):
+        with open('tarefas.txt', 'w') as f:
+            for x in self.lista_de_tarefas:
+                f.write(f'{x['status']}{x['nome']}\n')
+
+        
+
+
+    def exibir_menu(self):
+        print('''
+    ==============================
+        TRABALHADOR DA CASA
+    ==============================
+    1. Mostrar Lista
+    2. Adicionar Tarefa
+    3. Remover Tarefa
+    4. Concluir Tarefa
+    ------------------------------
+    0. Sair
+    ==============================
+    ''')
+        return input('Insira sua escolha: ')
+
---
+++ main.py
@@ -0,0 +1,24 @@
+from funcoes import Tarefa
+tarefa = Tarefa()
+escolha = None
+tarefa.trasncrever_lista()
+while True:
+
+    tarefa.atualizar_lista()
+    escolha = tarefa.exibir_menu()
+    if escolha not in ('0', '1', '2', '3', '4'):
+        print('\nInsira uma escolha existente no menu')
+    else:
+        if int(escolha) == 1:
+            tarefa.mostra_tarefas()
+        elif int(escolha) == 2:
+            tarefa.adicionar_tarefa()
+        elif int(escolha) == 3:
+            tarefa.remover_tarefas()
+        elif int(escolha) == 4:
+            tarefa.concluir_tarefa()
+        elif int(escolha) == 0:
+            break
+
+
+print('\nOBRIGADO PELO USO, NÃO ESQUEÇA SUAS TAREFAS...\n')
